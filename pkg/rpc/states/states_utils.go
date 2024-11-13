92d85189848785f80974c2ba148822c7cefd5017
pancsta <155631569+pancsta@users.noreply.github.com>
2024-11-13 08:51:52 +0100

    feat(rpc): add client failsafety

---
+++ pkg/rpc/states/states_utils.go
@@ -0,0 +1,22 @@
+package states
+
+import am "github.com/pancsta/asyncmachine-go/pkg/machine"
+
+// S is a type alias for a list of state names.
+type S = am.S
+
+// State is a type alias for a state definition.
+type State = am.State
+
+// SAdd is a func alias for merging lists of states.
+var SAdd = am.SAdd
+
+// StateAdd is a func alias for adding to an existing state definition.
+var StateAdd = am.StateAdd
+
+// StateSet is a func alias for replacing parts of an existing state
+// definition.
+var StateSet = am.StateSet
+
+// StructMerge is a func alias for extending an existing state structure.
+var StructMerge = am.StructMerge
