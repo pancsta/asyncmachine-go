ca41e9bdc68739b4b4756b9bcbfa2ab89cd9ddd2
pancsta <155631569+pancsta@users.noreply.github.com>
2024-11-13 14:10:39 +0100

    chore: clean up

---
+++ tools/debugger/server/dbg_rpc.go
@@ -0,0 +1,180 @@
+package server
+
+import (
+	"encoding/gob"
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/soheilhy/cmux"
+
+	am "github.com/pancsta/asyncmachine-go/pkg/machine"
+	"github.com/pancsta/asyncmachine-go/pkg/telemetry"
+	ss "github.com/pancsta/asyncmachine-go/tools/debugger/states"
+)
+
+type RPCServer struct {
+	Mach   *am.Machine
+	ConnID string
+}
+
+func (r *RPCServer) DbgMsgStruct(
+	msgStruct *telemetry.DbgMsgStruct, _ *string,
+) error {
+	r.Mach.Add1(ss.ConnectEvent, am.A{
+		"msg_struct": msgStruct,
+		"conn_id":    r.ConnID,
+		"Client.id":  msgStruct.ID,
+	})
+
+	return nil
+}
+
+var (
+	queue     []*telemetry.DbgMsgTx
+	queueMx   sync.Mutex
+	scheduled bool
+)
+
+func (r *RPCServer) DbgMsgTx(msgTx *telemetry.DbgMsgTx, _ *string) error {
+	queueMx.Lock()
+	defer queueMx.Unlock()
+
+	if !scheduled {
+		scheduled = true
+		go func() {
+			// debounce
+			time.Sleep(time.Second)
+
+			queueMx.Lock()
+			defer queueMx.Unlock()
+
+			r.Mach.Add1(ss.ClientMsg, am.A{"msgs_tx": queue})
+			// DEBUG
+			// println("sent", len(queue), "msgs")
+			queue = nil
+			scheduled = false
+		}()
+	}
+
+	now := time.Now()
+	msgTx.Time = &now
+	queue = append(queue, msgTx)
+
+	return nil
+}
+
+func StartRpc(mach *am.Machine, addr string, mux chan<- cmux.CMux) {
+	var err error
+	gob.Register(am.Relation(0))
+	if addr == "" {
+		addr = telemetry.DbgAddr
+	}
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Println(err)
+		mach.AddErr(err, nil)
+		// TODO nice err msg
+		panic(err)
+	}
+	mach.Log("dbg server started at %s", addr)
+
+	// cmux
+	m := cmux.New(lis)
+	// push out for IoC
+	if mux != nil {
+		mux <- m
+	}
+
+	rpcL := m.Match(cmux.Any())
+	go rpcAccept(rpcL, mach)
+
+	// start cmux
+	if err := m.Serve(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func rpcAccept(l net.Listener, mach *am.Machine) {
+	// TODO restart on error
+	defer mach.PanicToErr(nil)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+			mach.AddErr(err, nil)
+			continue
+		}
+
+		// handle the client
+		go func() {
+			server := rpc.NewServer()
+			connID := conn.RemoteAddr().String()
+			rcvr := &RPCServer{
+				Mach:   mach,
+				ConnID: connID,
+			}
+
+			err = server.Register(rcvr)
+			if err != nil {
+				log.Println(err)
+				rcvr.Mach.AddErr(err, nil)
+				// TODO err msg
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			server.ServeConn(conn)
+			rcvr.Mach.Add1(ss.DisconnectEvent, am.A{"conn_id": connID})
+		}()
+	}
+}
+
+// ///// ///// /////
+
+// ///// REMOTE MACHINE RPC
+
+// ///// ///// /////
+
+type GetField int
+
+const (
+	GetCursorTx GetField = iota + 1
+	GetCursorStep
+	GetClientCount
+	GetMsgCount
+	GetOpts
+	GetSelectedState
+)
+
+func (n GetField) String() string {
+	switch n {
+	case GetCursorTx:
+		return "GetCursorTx"
+	case GetCursorStep:
+		return "GetCursorStep"
+	case GetClientCount:
+		return "GetClientCount"
+	case GetMsgCount:
+		return "GetMsgCount"
+	case GetOpts:
+		return "GetOpts"
+	case GetSelectedState:
+		return "GetSelectedState"
+	}
+
+	return "!UNKNOWN!"
+}
+
+func (n GetField) Encode() string {
+	return string(rune(n))
+}
+
+func Decode(s string) GetField {
+	return GetField(s[0])
+}
---
+++ tools/debugger/utils_test.go
@@ -0,0 +1,81 @@
+package debugger
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHumanSort(t *testing.T) {
+	data := []string{
+		"_nco-dev-test-210946-00e4ff1",
+		"_rs-nco-dev-test-210946-00e4ff1",
+		"_rc-con-dev-test-2870ef",
+		"_rs-nw-loc-dev-test-6791e5",
+		"_rc-con-dev-test-6791e5",
+		"_ns-dev-test-210946-0",
+		"_rs-ns-pub-dev-test-210946-0",
+		"_rs-ns-loc-dev-test-210946-0",
+		"_rs-nco-dev-test-210946-0e7608f",
+		"_rc-ns-dev-test-p-36317",
+		"_rs-nw-loc-dev-test-b55a1b",
+		"_nco-dev-test-210946-0c2bbae",
+		"_rc-ns-dev-test-p-44331",
+		"_rs-nw-loc-dev-test-2870ef",
+		"_nw-dev-test-2870ef",
+		"_nw-dev-test-6791e5",
+		"_nco-dev-test-210946-0e7608f",
+		"_nw-dev-test-b55a1b",
+		"_rc-con-dev-test-b55a1b",
+		"_rs-nco-dev-test-210946-0c2bbae",
+	}
+
+	expected := []string{
+		"_nco-dev-test-210946-00e4ff1",
+		"_nco-dev-test-210946-0c2bbae",
+		"_nco-dev-test-210946-0e7608f",
+
+		"_ns-dev-test-210946-0",
+
+		"_nw-dev-test-2870ef",
+		"_nw-dev-test-6791e5",
+		"_nw-dev-test-b55a1b",
+
+		"_rc-con-dev-test-2870ef",
+		"_rc-con-dev-test-6791e5",
+		"_rc-con-dev-test-b55a1b",
+		"_rc-ns-dev-test-p-36317",
+		"_rc-ns-dev-test-p-44331",
+		"_rs-nco-dev-test-210946-00e4ff1",
+		"_rs-nco-dev-test-210946-0c2bbae",
+		"_rs-nco-dev-test-210946-0e7608f",
+		"_rs-ns-loc-dev-test-210946-0",
+		"_rs-ns-pub-dev-test-210946-0",
+		"_rs-nw-loc-dev-test-2870ef",
+		"_rs-nw-loc-dev-test-6791e5",
+		"_rs-nw-loc-dev-test-b55a1b",
+	}
+
+	humanSort(data)
+	join := strings.Join(data, "\n")
+
+	// debug
+	// t.Log("\n" + join)
+
+	assert.Equal(t, strings.Join(expected, "\n"), join)
+}
+
+func TestHadErrSince(t *testing.T) {
+	c := &Client{
+		errors: []int{100, 50, 5, 1},
+	}
+
+	assert.False(t, c.hadErrSinceTx(300, 5), "tx: %d, dist: %d", 300, 5)
+	assert.False(t, c.hadErrSinceTx(105, 3), "tx: %d, dist: %d", 105, 3)
+
+	assert.True(t, c.hadErrSinceTx(55, 10), "tx: %d, dist: %d", 55, 10)
+	assert.True(t, c.hadErrSinceTx(6, 2), "tx: %d, dist: %d", 6, 2)
+	assert.True(t, c.hadErrSinceTx(100, 2), "tx: %d, dist: %d", 100, 2)
+	assert.True(t, c.hadErrSinceTx(1, 1), "tx: %d, dist: %d", 1, 1)
+}
