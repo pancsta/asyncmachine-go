36e3454bafd9794cd1f952375187911e38a30b7d
pancsta <155631569+pancsta@users.noreply.github.com>
2024-11-19 16:12:51 +0100

    ci: add tests and LoC badges (#155)

---
+++ scripts/compact_number/compact_number.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/nkall/compactnumber"
+)
+
+func main() {
+	var input string
+
+	if len(os.Args) > 1 {
+		input = os.Args[1]
+	} else {
+		fmt.Scanln(&input) //nolint:errcheck
+	}
+
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Printf("Invalid number: %s\n", input)
+		return
+	}
+
+	formatter := compactnumber.NewFormatter("en-US", compactnumber.Short)
+	out, err := formatter.Format(number)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(out)
+}
